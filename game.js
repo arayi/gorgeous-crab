709bffd1d0cc77bdc460edf9bf6ec9bd65a24cc5
Melody Mustaine <mel.mustaine@gmail.com>
2016-09-20 12:35:35 -0700

    add basic game logic

--- game.js
+++ game.js
@@ -1,3 +1,23 @@
 var english = require("an-array-of-english-words");
 var spanish = require("an-array-of-spanish-words");
 var french = require("an-array-of-french-words");
+
+function gameMain (language, difficulty) {
+  var settings = [language, difficulty];
+  var score = 0;
+};
+
+function generateWord (settings) {
+  var language = settings[0];
+  var difficulty = settings[1];
+
+
+};
+
+function shuffleLetters (currentWord) {
+
+};
+
+function isCorrect (answer, word) {
+ return answer.toLowercase() === word.toLowercase();
+};
