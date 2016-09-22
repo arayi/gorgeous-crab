c3e7489bd70aacff451577d6b7cc67fef90c8a76
Melody Mustaine <mel.mustaine@gmail.com>
2016-09-22 11:50:16 -0700

    added testing dictionaries

---
+++ mots.js
@@ -0,0 +1,5 @@
+const dict = {
+  'easy': ['fear', 'fall', 'fart', 'futt', 'foot'],
+  'medium': ['fello', 'forld', 'footh', 'frase', 'fiker'],
+  'hard': ['fursue', 'findow', 'fotato', 'fepper', 'fallen']
+}
---
+++ palabras.js
@@ -0,0 +1,5 @@
+const dict = {
+  'easy': ['sear', 'sall', 'sart', 'sutt', 'soot'],
+  'medium': ['sello', 'sorld', 'sooth', 'srase', 'siker'],
+  'hard': ['sursue', 'sindow', 'sotato', 'sepper', 'sallen']
+}
